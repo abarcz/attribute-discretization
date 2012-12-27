51dc99b412c16a7b5baa8083a847110424e8d739
Aleksy Barcz <abarcz@gmail.com>
2012-12-27 13:15:15 +0100

    Google Coding Style

--- src/evaltree.R
+++ src/evaltree.R
@@ -1,55 +1,55 @@
 library(rpart)
 
-EvalTree <- function(train_set, test_set, treshold) {
+EvalTree <- function(train.set, test.set, treshold) {
 	# build tree model, predicting the V1 attribute, basing on the rest
-	model <- rpart(V1 ~ ., method="class", data=train_set)
-	probabilities <- predict(model, test_set[,-1])
+	model <- rpart(V1 ~ ., method="class", data=train.set)
+	probabilities <- predict(model, test.set[, -1])
 
 	# select classes with max probabilities
-	max_prob <- apply(probabilities, 1, max)
-	idx <- which(probabilities == max_prob, arr.ind=TRUE)
-	no_decision_val <- 0
+	max.prob <- apply(probabilities, 1, max)
+	idx <- which(probabilities == max.prob, arr.ind=TRUE)
+	no.decision.val <- 0
 
-	# if two decision has the same probability, mark as no_decision
-	n_non_unique_class <- 0
+	# if two decision has the same probability, mark as no.decision
+	n.non.unique.class <- 0
 	classes <- matrix(nrow=nrow(probabilities), ncol=1)
 	for (i in 1:nrow(probabilities)) {
-		ith_class_entry <- idx[which(idx[, 1] == i), ]
-		if (length(ith_class_entry) > 2) {
-			n_non_unique_class <- n_non_unique_class + 1
-			ith_class <- no_decision_val
+		ith.class.entry <- idx[which(idx[, 1] == i), ]
+		if (length(ith.class.entry) > 2) {
+			n.non.unique.class <- n.non.unique.class + 1
+			ith.class <- no.decision.val
 		} else {
-			ith_class <- ith_class_entry[2]
+			ith.class <- ith.class.entry[2]
 		}
-		classes[i] <- ith_class
+		classes[i] <- ith.class
 	}
 	print("Number of non-unique maxima:")
-	print(n_non_unique_class)
+	print(n.non.unique.class)
 
 	# apply no-decision treshold
-	no_decision_selector <- max_prob < treshold
-	classes[no_decision_selector] <- no_decision_val
+	no.decision.selector <- max.prob < treshold
+	classes[no.decision.selector] <- no.decision.val
 
-	correct_selector <- classes == test_set$V1
-	correct_num <- sum(correct_selector == TRUE)
-	no_decision_num <- sum(no_decision_selector == TRUE)
-	incorrect_num <- sum(correct_selector == FALSE) - no_decision_num
-	return(c(correct_num, no_decision_num, incorrect_num))
+	correct.selector <- classes == test.set$V1
+	correct.num <- sum(correct.selector == TRUE)
+	no.decision.num <- sum(no.decision.selector == TRUE)
+	incorrect.num <- sum(correct.selector == FALSE) - no.decision.num
+	return(c(correct.num, no.decision.num, incorrect.num))
 }
 
-CrossValidateTree <- function(dataset, n_folds, treshold) {
-	id <- sample(1:n_folds, nrow(dataset), replace=TRUE)
+CrossValidateTree <- function(dataset, n.folds, treshold) {
+	id <- sample(1:n.folds, nrow(dataset), replace=TRUE)
 	#ListX <- split(X,id) # gives you a list with the 5 matrices
 
-	results_matrix <- matrix(nrow=n_folds, ncol=3)
-	for (i in 1:n_folds) {
-		test_set <- dataset[id == i,]
-		train_set <- dataset[id != i,]
-		results <- EvalTree(train_set, test_set, treshold)
-		results_matrix[i,] <- results
+	results.matrix <- matrix(nrow=n.folds, ncol=3)
+	for (i in 1:n.folds) {
+		test.set <- dataset[id == i, ]
+		train.set <- dataset[id != i, ]
+		results <- EvalTree(train.set, test.set, treshold)
+		results.matrix[i, ] <- results
 	}
-	scores <- results_matrix / apply(results_matrix, 1, sum)
+	scores <- results.matrix / apply(results.matrix, 1, sum)
 	means <- apply(scores, 2, mean)
 	stds <- apply(scores, 2, sd)
-	return(rbind(means,stds))
+	return(rbind(means, stds))
 }
