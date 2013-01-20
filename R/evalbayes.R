ddfb78dc092bd3bb59b322c604c7fb4fd7bfde0b
Aleksy Barcz <abarcz@gmail.com>
2013-01-20 14:32:55 +0100

    EvalBayes: build on train, discretize on test

--- R/evalbayes.R
+++ R/evalbayes.R
@@ -6,7 +6,21 @@ PrepareDataset <- function(path) {
 	return(data)
 }
 
-EvaluateBayes <- function(formula, train.set, test.set) {
+EvaluateBayes <- function(formula, train.set, test.set, discretization.method,
+		discretization.stop.criterion) {
+
+	if (discretization.method == "TopDown") {
+		discretization <- TopDown(formula, train.set, discretization.stop.criterion)
+	} else if (discretization.method == "BottomUp") {
+		discretization <- BottomUp(formula, train.set, discretization.stop.criterion)
+	} else if (discretization.method != "None") {
+		stop("Invalid discretization.method, must be 'TopDown', 'BottomUp' or 'None'")
+	}
+	if (discretization.method != "None") {
+		train.set <- predict(discretization, train.set)
+		test.set <- predict(discretization, test.set)
+	}
+
 	classifier <- naiveBayes(formula, train.set)
 	predicted <- predict(classifier, test.set[, -1])
 	confusion.matrix <- table(predicted, test.set[, 1])
@@ -14,14 +28,17 @@ EvaluateBayes <- function(formula, train.set, test.set) {
 	return(accuracy)
 }
 
-CrossValidateBayes <- function(formula, dataset, n.folds) {
+CrossValidateBayes <- function(formula, dataset, n.folds,
+		discretization.method, discretization.stop.criterion) {
+
 	id <- sample(1:n.folds, nrow(dataset), replace=TRUE)
 
 	results.matrix <- matrix(nrow=n.folds, ncol=1)
 	for (i in 1:n.folds) {
 		test.set <- dataset[id == i, ]
 		train.set <- dataset[id != i, ]
-		results.matrix[i, 1] <- EvaluateBayes(formula, train.set, test.set)
+		results.matrix[i, 1] <- EvaluateBayes(formula, train.set, test.set,
+									discretization.method, discretization.stop.criterion)
 	}
 	scores <- results.matrix
 	means <- apply(scores, 2, mean)
