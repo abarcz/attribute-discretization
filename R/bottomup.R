5ccd3fb927f8c435af5d163af20702d1db6b79b2
W.Koszolko <W.Koszolko@gmail.com>
2013-01-21 19:15:43 +0100

    bottomup

--- R/bottomup.R
+++ R/bottomup.R
@@ -4,7 +4,7 @@ source("R/intervals_num_criterion.R")
 source("R/bu_min_chi_criterion.R")
 
 BottomUp <- function(formula, data, stop.criterions=RequestedIntervalsNumCriterion(5)) {
-	# Computes the top-down discretization of given data.
+	# Computes the bottom-up discretization of given data.
 	#
 	# Args:
 	#	formula: a formula, selecting args for discretization
@@ -19,7 +19,7 @@ BottomUp <- function(formula, data, stop.criterions=RequestedIntervalsNumCriteri
 
 	for (stop.criterion in model$stop.criterions) {
 		if (!("BottomUpStopCriterion" %in% class(stop.criterion))) {
-			stop("Argument 'stop.criterion' is not a TopDownStopCriterion")
+			stop("Argument 'stop.criterion' is not a BottomUpStopCriterion")
 		}
 	}
 
@@ -40,28 +40,18 @@ DiscretizeAttribute.BottomUp <- function(object, attribute.name) {
 	data <- CreateSlice(object, attribute.name)
 	result.split.points <- c()
 	split.points <- CreateInintialSplitPoints(data)
-	print("initial split points")
-	print(split.points)
 	intervals <- CreateIntervals(data, split.points)
-	print("initial intervals")
-	print(intervals)
 	chi.vector <- CalculateChiForAllIntervals(intervals)
 	minChi = min(chi.vector)
-	print(paste("initial minChi", minChi))
 	while (!StopCriterionSatisfied(object, intervals,minChi) && length(intervals)>1) {
 		result.split.points <- c(split.points)
-		print(paste("result.split.points", result.split.points))
+		
 		chi.vector <- CalculateChiForAllIntervals(intervals)
 
 		minChi = min(chi.vector)
-		print(paste("minChi",minChi))
 		index.split.poin.to.merge = order(minChi)[1]
 		split.points <- MergeSplitPoints(split.points, index.split.poin.to.merge)
-		print("create intervals")
-		print("data")		
-		print(data)
-		print("split.points")
-		print(split.points)
+		
 		intervals <- CreateIntervals(data, split.points)
 		if(length(intervals)==1) {
 			print("Warning: no more intervals to merge!")
@@ -124,9 +114,8 @@ CalculateChiForAllIntervals <- function(intervals) {
 	
 MergeSplitPoints <- function(split.points, index.split.poin.to.merge) {
 	new.split.points <- c()
-	print(paste("length split points", length(split.points)))
+	
 	if(length(split.points)==1) {
-		print("return c()")
 		return (new.split.points)
 	}
 
@@ -153,40 +142,24 @@ CalculateChi <-function(interval1, interval2) {
 	chi<-0
 	labels <- c(unique(interval1$labels), unique(interval2$labels))
 	labels <- unique(labels)
-	print(labels)
 
 	for(label in labels) {
 		A1j <- 0
 		A2j <- 0
-		print(paste("j",label))
 		selector <- interval1$labels == label
-		print(paste("selector",selector))
 		A1j <- length(interval1[selector,]$attr)
-		print(paste("A1j",A1j))
 		selector <- interval2$labels == label
-		print(paste("selector",selector))
 		A2j <- length(interval2[selector,]$attr)
-		print(paste("A2j",A2j))
 		R1 <- length(interval1$attr)
-		print(paste("R1 ",R1))
 		R2 <- length(interval2$attr)
-		print(paste("R2 ",R2))
 		Cj <- A1j + A2j
-		print(paste("Cj ",Cj))
 		N <- R1 + R2
-		print(paste("N ",N))
 		E1j <- (R1*Cj)/N
-		print(paste("E1j ",E1j))
 		E2j <- (R2*Cj)/N
-		print(paste("E2j ",E2j))
 		tmpSum1j <- (((A1j-E1j)^2)/E1j)
 		sum1 <- sum1+tmpSum1j
-		print(paste("tmpSum1j",tmpSum1j))
-		print(paste("sum1 ",sum1))
 		tmpSum2j <- (((A2j-E2j)^2)/E2j)
 		sum2 <- sum2+tmpSum2j
-		print(paste("tmpSum2j",tmpSum2j))
-		print(paste("sum2 ",sum2))
 		chi <- tmpSum1j + tmpSum2j + chi
 	}
 	return(chi)
